45855c90c58aad8324a59fb2cde356fb90bde87b
UneasePurse37 <67396532+Unease@users.noreply.github.com>
2022-03-10 18:24:10 -0800

    Init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-PeacefulRNG
+# PeacefulRNG
+A mod that removes the hunger status effect randomness
+from rotten flesh, raw chicken, and poisonous potatos
+## Compatible Versions
+1.12.2
---
+++ src/main/java/me/unease/PeacefulRNG/FoodEatEvent.java
@@ -0,0 +1,22 @@
+package me.unease.PeacefulRNG;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author UneasePurse37
+ */
+
+@Mod(modid = "peacefulrng", name = "PeacefulRNG", version = "1.0.0")
+
+public class FoodEatEvent{
+
+	/**
+	 * Called whenever a player eats.
+	 * Changes probabilities of certain foods giving certain effects.
+	 * 
+	 */
+	public void onEatFood() {
+		
+		
+	}
+}
