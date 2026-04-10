469aa73a548137f3bed7edcec449d1d5bd5f3d8c
Noah Tung <uneasepurse37@gmail.com>
2026-04-10 15:19:22 -0700

    Modified redirects to use WrapOperation

--- src/main/java/com/minecrafttas/peacefulrng/mixin/MixinItemFood.java
+++ src/main/java/com/minecrafttas/peacefulrng/mixin/MixinItemFood.java
@@ -2,10 +2,10 @@ package com.minecrafttas.peacefulrng.mixin;
 
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.minecrafttas.peacefulrng.PeacefulRNG;
 
 import net.minecraft.item.ItemFood;
@@ -13,17 +13,12 @@ import net.minecraft.item.ItemFood;
 //# 1.11.2
 @Mixin(ItemFood.class)
 //# end
-
 public class MixinItemFood {
+	
 	//# 1.11.2
-	@Shadow
-	private float potionEffectProbability;
+	@WrapOperation(method = "onFoodEaten", at = @At(value = "FIELD", target = "Lnet/minecraft/item/ItemFood;potionEffectProbability:F", opcode = Opcodes.GETFIELD))
 	//# end
-
-	//# 1.11.2
-	@Redirect(method = "onFoodEaten", at = @At(value = "FIELD", target = "Lnet/minecraft/item/ItemFood;potionEffectProbability:F", opcode = Opcodes.GETFIELD))
-	//# end
-	private float effectChanceValueRedirect(ItemFood food) {
+	private float receiveEffectChance(ItemFood food, Operation<Float> original) {
 		//# 1.12.2
 //$$		String name = food.getTranslationKey();
 		//# 1.11.2
@@ -32,17 +27,11 @@ public class MixinItemFood {
 		String rottenFlesh = "item.rottenFlesh";
 		String rawChicken = "item.chickenRaw";
 		String potatoPoisonous = "item.potatoPoisonous";
-		
 		if (PeacefulRNG.isTASMODLoaded) {
 			if (name.equalsIgnoreCase(rottenFlesh) || name.equalsIgnoreCase(potatoPoisonous) || name.equalsIgnoreCase(rawChicken)) {
-				//# 1.11.2
-				potionEffectProbability = 0F;
-				return potionEffectProbability;
-				//# end
+				return 0F; // They will never give the effect because its RNG
 			}
-		}
-		//# 1.11.2
-		return potionEffectProbability;
-		//# end
+		} 
+		return original.call(food);
 	}
 }
